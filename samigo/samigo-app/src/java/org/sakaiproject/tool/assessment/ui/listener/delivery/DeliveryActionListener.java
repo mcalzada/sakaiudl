39a448752a6a8ded53d94086bb9d363afaf565ce
Francisco Saez Segura <francisco.saez.segura@gmail.com>
2023-10-17 15:27:13 +0200

    S2U-16: Tests & Quizzes: Time limit per question or page (#11952)
    S2U-16: Tests & Quizzes: Time limit per question or page --- Afegim canvis morpheus-udl
    
    S2U-16: Samigo bugfixes related to timers  (#12045)
    
    S2U-16 Add missing EU translations (#12305)

--- samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
+++ samigo/samigo-app/src/java/org/sakaiproject/tool/assessment/ui/listener/delivery/DeliveryActionListener.java
@@ -62,6 +62,7 @@ import org.sakaiproject.tool.assessment.data.dao.assessment.PublishedItemFeedbac
 import org.sakaiproject.tool.assessment.data.dao.grading.AssessmentGradingData;
 import org.sakaiproject.tool.assessment.data.dao.grading.ItemGradingAttachment;
 import org.sakaiproject.tool.assessment.data.dao.grading.ItemGradingData;
+import org.sakaiproject.tool.assessment.data.dao.grading.SectionGradingData;
 import org.sakaiproject.tool.assessment.data.ifc.assessment.AnswerIfc;
 import org.sakaiproject.tool.assessment.data.ifc.assessment.AssessmentAccessControlIfc;
 import org.sakaiproject.tool.assessment.data.ifc.assessment.AssessmentFeedbackIfc;
@@ -926,6 +927,7 @@ public class DeliveryActionListener
       }
     }
 
+    delivery.setNextEnabled(true);
     contents.setCurrentScore(currentScore);
     contents.setMaxScore(maxScore);
     contents.setPartsContents(partsContents);
@@ -1014,6 +1016,12 @@ public class DeliveryActionListener
           delivery.setPrevious(true);
         } else {
           delivery.setPrevious(false);
+        }
+        //linear access
+        if (DeliveryBean.LINEAR_ACCESS.equals(delivery.getNavigation())) {
+          delivery.setNextEnabled(partBeanWithQuestion.getEnabled() >= 0);
+        } else { //non-linear access
+          delivery.setNextEnabled(true);
         }
       }
     }
@@ -1105,6 +1113,10 @@ public class DeliveryActionListener
     sec.setUnansweredQuestions(unansweredQuestions);
     sec.setItemContents(itemContents);
     sec.setAttachmentList(part.getSectionAttachmentList());
+
+    GradingService gs = new GradingService();
+    sec.setSectionGradingData(gs.getSectionGradingData(delivery.getAssessmentGradingId(), Long.parseLong(sec.getSectionId()), AgentFacade.getAgentString()));
+
     return sec;
   }
 
@@ -1189,6 +1201,9 @@ public class DeliveryActionListener
     sec.setShowStudentQuestionScore(delivery.isShowStudentQuestionScore());
     sec.setUnansweredQuestions(unansweredQuestions);
     sec.setItemContents(itemContents);
+
+    GradingService gs = new GradingService();
+    sec.setSectionGradingData(gs.getSectionGradingData(delivery.getAssessmentGradingId(), Long.parseLong(sec.getSectionId()), AgentFacade.getAgentString()));
 
     return sec;
   }
