0fee13a545e8a576f5ec09bce95ff153dd7117bd
Mateu Llas Rubio <mateu.llas@udl.cat>
2025-07-01 12:44:48 +0200

    Revert "EVDOC01-178 portal 2 - Especific UdL"
    This reverts commit 8bcab1d63e5dae22eafa7d570aeef65cde111294.

--- portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
+++ portal/portal-impl/impl/src/java/org/sakaiproject/portal/charon/handlers/SiteHandler.java
@@ -676,7 +676,7 @@ public class SiteHandler extends WorksiteHandler
 		if (userId != null) {
 			String skin = getSiteSkin(siteId);
 			String quickLinksTitle = portalService.getQuickLinksTitle(skin);
-			List<Map> quickLinks = portalService.getQuickLinks(skin, userId);
+			List<Map> quickLinks = portalService.getQuickLinks(skin);
 			if (CollectionUtils.isNotEmpty(quickLinks)) {
 				rcontext.put("quickLinksInfo", quickLinksTitle);
 				rcontext.put("quickLinks", quickLinks);
