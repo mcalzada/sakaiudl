7e118fcc496fe1752aef62d284a54c8b2f927fa4
Mateu Llas Rubio <mateu.llas@udl.cat>
2025-10-02 18:35:02 +0200

    EVDOC01-206  Prova canvi idioma motd

--- library/src/webapp/js/portal/portal.changeIdiom.js
+++ library/src/webapp/js/portal/portal.changeIdiom.js
@@ -1,21 +1,52 @@
 function selectContentFromIdiom() {
-   
-    const lang = document.documentElement.getAttribute('lang');
-    console.log(lang);
-    let frameObj = document.getElementsByClassName('Mrphs-toolBody Mrphs-toolBody--sakai-motd')[0].firstElementChild;
-    let contenidors = frameObj.
-        contentWindow.document.getElementsByClassName('contingutsMOTD');;
-    console.log(contenidors);
-    for (var i = 0; i < contenidors.length; i++) 
-                   if (contenidors[i].id === lang) {
-                       console.log("Tracto "+contenidors[i].id);
-                       contenidors[i].style.display="block";
-                       }
-               else {
-                contenidors[i].style.display="none";
-               }
+	const lang = document.documentElement.getAttribute('lang');
+	console.log(lang);
+	const elements = document.getElementsByClassName('Mrphs-container Mrphs-sakai-motd');
+	console.log(elements);
+	if (elements.length > 0) {
+		for (var i = 0; i < elements.length; i++) {
+			var contenidors = elements[i].getElementsByClassName('contingutsMOTD');
+			if (contenidors.length > 0) {
+				console.log(contenidors);
+				for (var i = 0; i < contenidors.length; i++) {
+					if (contenidors[i].id === lang) {
+						console.log("Mostrem continguts "+contenidors[i].id);
+						contenidors[i].style.display="block";
+					}
+					else {
+						contenidors[i].style.display="none";
+               		}
+				}
+    		}
+			else {
+				// dins iframe no es veuen
+				console.log("iframe");
+				var contenidorsIframe = elements[i].getElementsByClassName('Mrphs-toolBody Mrphs-toolBody--sakai-motd');
+				if (contenidorsIframe.length > 0) {
+					let frameObj = contenidorsIframe[0].firstElementChild;
+					let contenidorsMOTD = 	frameObj.contentWindow.document.getElementsByClassName('contingutsMOTD');
+					console.log(contenidorsMOTD);	
+					for (var i = 0; i < contenidorsMOTD.length; i++) {
+						if (contenidorsMOTD[i].id === lang) {
+							console.log("Mostrem continguts "+contenidorsMOTD[i].id);
+							contenidorsMOTD[i].style.display="block";
+                		}
+						else {
+							contenidorsMOTD[i].style.display="none";
+               			}
+            		}
+				}
+				else {
+						console("Sense continguts MOTD dins iframe");
+				}
+
+			}
+		}
+	}
+	else {
+		console.log("No hi ha elements amb la classe Mrphs-container Mrphs-sakai-motd");
+	}
 }
 window.onload = function() {
-    selectContentFromIdiom();
+	selectContentFromIdiom();
 };
-
